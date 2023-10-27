98f945191f2098073c1bfd7a54b88d4934aeca33
devsoojin <dev.soojin.an@gmail.com>
2023-10-28 00:41:43 +0900

    feat: add main page

--- soojin/pages/index.tsx
+++ soojin/pages/index.tsx
@@ -1,6 +1,51 @@
 
 export default function Home() {
   return (
-    <main className="font-[NPS] text-[Green]">Hello World</main>
+    <main className="font-[NPS] mt-[4rem]">
+      <div>
+        <div className="text-2xl font-[NPSBold] w-[20%] text-mainYellow mb-3">About Myself</div>
+        <div className="border p-3 rounded-md leading-8">
+          I am a fervent <b className="text-mainRed">machine learning engineer</b>, presently contributing my expertise to <b className="text-mainRed">PwC Korea</b>. My professional journey has allowed me to merge my profound passion for <b className="text-mainRed">MLOps</b> with the esteemed legacy of a global industry leader. As a proud member of the PwC Korea team, I have the privilege of collaborating with brilliant minds and leveraging cutting-edge technology to drive innovation and transform the landscape of data-driven decision-making. This dynamic role allows me to not only explore the depths of AI but also apply my skills to solve complex, real-world challenges. With a relentless enthusiasm for my work and a penchant for infusing positive energy into every project, I am poised to continue shaping the future of machine learning and MLOps.
+        </div>
+      </div>
+      <div className="mt-10 ">
+        <div className="text-2xl font-[NPSBold] w-[20%] text-mainYellow mb-3">Career</div>
+        <div className="border p-3 rounded-md leading-9">
+        <div className="flex w-full ">
+          <div className="w-[12vw]">
+            2019.03 ~ 2023.02
+          </div>
+          <div>
+            Korea University ( International Studies & Computer Science )
+          </div>
+           
+        </div>
+        <div className="flex w-full ">
+          <div className="w-[12vw]">
+            2021.06 ~ 2021.08
+          </div>
+          <div>
+            Software Internship at IoTCube
+          </div>
+        </div>
+        <div className="flex w-full ">
+          <div className="w-[12vw]">
+            2021.09 ~ 2022.08
+          </div>
+          <div>
+            Machine Learning Engineer at Corca AI
+          </div>
+        </div>
+        <div className="flex w-full ">
+          <div className="w-[12vw]">
+            2023.01 ~ 
+          </div>
+          <div>
+            Machine Learning Engineer at PwC Korea (삼일회계법인)
+          </div>
+        </div>
+        </div>
+      </div>
+    </main>
   )
 }
